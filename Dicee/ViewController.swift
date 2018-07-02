ad26cc3e7acd86ae7a2e368fde30d5cc1b128fc1
Gursharnbir Singh <gursharnbirsingh@gursharnbirsmbp>
2018-07-02 17:09:09 -0600

    add UI elements and view controller

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex:Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
